438b64ea9964b2cb8d845753ae91eac967ad0d48
zhangxinnan <zhangxin19870504@163.com>
2019-05-16 19:52:14 +0800

    add sensors

---
+++ Android/FirstLineCode/ch10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -0,0 +1,48 @@
+package com.example.servicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    private DownloadBinder mBinder = new DownloadBinder();
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d("MyService", "startDownload executed");
+        }
+        public int getProgress() {
+            Log.d("MyService", "getProgress executed");
+            return 0;
+        }
+    }
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService", "onCreate executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyService", "onDestroy executed");
+    }
+}
