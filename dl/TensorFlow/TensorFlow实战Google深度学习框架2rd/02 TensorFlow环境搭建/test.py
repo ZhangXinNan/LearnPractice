e443de9d0fe4f52bda34ec2b78f59991e649458a
zhangxin <zhangxin19870504@163.com>
2020-02-08 15:32:48 +0800

    test.py test tf

---
+++ dl/TensorFlow/TensorFlow实战Google深度学习框架2rd/02 TensorFlow环境搭建/test.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+a = tf.constant([1, 2.0], name='a')
+b = tf.constant([3.0, 4.0], name='b')
+c = tf.add(a, b)
+
+print(a)
+print(b)
+print(c)
+
+with tf.Session() as sess:
+    aa, bb, cc = sess.run([a, b, c])
+    print(a)
+    print(b)
+    print(c)
+    print(aa, bb, cc)
