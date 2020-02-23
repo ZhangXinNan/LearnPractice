bf1f5d3cde66746d66122dd56b3389ef790794e0
zhangxinnan <zhangxin19870504@163.com>
2020-02-24 04:04:42 +0800

    pil 从字节流中读取图片

--- cv/PIL/read_write.py
+++ cv/PIL/read_write.py
@@ -3,13 +3,13 @@ from PIL import Image
 
 img_path = './car.jpg'
 # 从内存中读图片
-stream = open(img_path, 'rb').read()
-img = Image.open(StringIO(content))
+content = open(img_path, 'rb').read()
+img = Image.open(BytesIO(content))
 print(img.size, img.mode)
 
 # 往内存中写图片
 img = Image.open(img_path)
-with BytesIO as out:
+with BytesIO() as out:
     img.save(out, format='JPEG')
     with open('out_stream.jpg', 'wb') as fo:
         fo.write(out.getvalue())
