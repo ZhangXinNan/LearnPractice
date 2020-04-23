3fe1e597ec87e7b46ea234aed0341a4356642907
zhangxinnan <zhangxin19870504@163.com>
2020-04-23 14:20:01 +0800

    runoob threading.thread

---
+++ python/libs/thread/runoob/demo00.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import _thread
+import time
+
+# 为线程定义一个函数
+def print_time( threadName, delay):
+   count = 0
+   while count < 5:
+      time.sleep(delay)
+      count += 1
+      print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
+
+# 创建两个线程
+try:
+   _thread.start_new_thread( print_time, ("Thread-1", 2, ) )
+   _thread.start_new_thread( print_time, ("Thread-2", 4, ) )
+except:
+   print ("Error: 无法启动线程")
+
+while 1:
+   pass
--- python/libs/thread/runoob/demo02.py
+++ python/libs/thread/runoob/demo02.py
@@ -3,8 +3,6 @@
 import threading
 import time
 
-exitFlag = 0
-
 class myThread (threading.Thread):
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
@@ -12,17 +10,21 @@ class myThread (threading.Thread):
         self.name = name
         self.counter = counter
     def run(self):
-        print ("开始线程：" + self.name)
-        print_time(self.name, self.counter, 5)
-        print ("退出线程：" + self.name)
+        print ("开启线程： " + self.name)
+        # 获取锁，用于线程同步
+        threadLock.acquire()
+        print_time(self.name, self.counter, 3)
+        # 释放锁，开启下一个线程
+        threadLock.release()
 
 def print_time(threadName, delay, counter):
     while counter:
-        if exitFlag:
-            threadName.exit()
         time.sleep(delay)
         print ("%s: %s" % (threadName, time.ctime(time.time())))
         counter -= 1
+
+threadLock = threading.Lock()
+threads = []
 
 # 创建新线程
 thread1 = myThread(1, "Thread-1", 1)
@@ -31,9 +33,12 @@ thread2 = myThread(2, "Thread-2", 2)
 # 开启新线程
 thread1.start()
 thread2.start()
-thread1.join()
-thread2.join()
+
+# 添加线程到线程列表
+threads.append(thread1)
+threads.append(thread2)
+
+# 等待所有线程完成
+for t in threads:
+    t.join()
 print ("退出主线程")
-
-
-# join 到底有什么用？
