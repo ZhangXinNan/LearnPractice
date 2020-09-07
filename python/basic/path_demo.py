092413f4e6737311c7ad65ffdef026f3aca449a8
zhangxin <zhangxin19870504@163.com>
2020-09-07 09:28:01 +0800

    pathlib

--- python/basic/path_demo.py
+++ python/basic/path_demo.py
@@ -16,3 +16,14 @@ dir_name = os.path.dirname(os.path.abspath(__file__))
 print(dir_name)
 
 print(os.path.abspath(os.path.join(dir_name, '../../')))
+
+
+from pathlib import Path
+# 当前文件的绝对路径
+__dir__ = Path(__file__).resolve()
+print(type(__dir__), __dir__)
+str_dir = str(__dir__)
+print(type(str_dir), str_dir)
+
+
+
