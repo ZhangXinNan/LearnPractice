07bd658ed74945b688b88afc37f05169cccc89de
zhangxin <zhangxin19870504@163.com>
2021-12-05 23:27:34 +0800

    test pytorch 1.8.2 cuda 10.2 cudnn in win10

--- Notes/Python深度学习基于PyTorch/ch06/cifar.py
+++ Notes/Python深度学习基于PyTorch/ch06/cifar.py
@@ -1,4 +1,4 @@
-
+import torch
 import torch.nn as nn
 
 from load_data import *
---
+++ dl/pytorch/test_pytorch.py
@@ -0,0 +1,14 @@
+
+import torch
+print(torch.__version__)
+print('CUDA available: ' + str(torch.cuda.is_available()))
+print('cuDNN version: ' + str(torch.backends.cudnn.version()))
+a = torch.cuda.FloatTensor(2).zero_()
+print('Tensor a = ' + str(a))
+b = torch.randn(2).cuda()
+print('Tensor b = ' + str(b))
+c = a + b
+print('Tensor c = ' + str(c))
+
+import torchvision
+print(torchvision.__version__)
