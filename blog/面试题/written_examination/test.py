ba7f073064aa034931ff085ab8923c36a8520ecf
zhangxinnan <zhangxin19870504@163.com>
2022-06-28 16:17:25 +0800

    调整目录结构

--- blog/面试题/written_examination/test.py
+++ blog/面试题/written_examination/test.py
@@ -4,11 +4,11 @@ flag = [0] * (len(a) + 1)
 for i,v in enumerate(a):
     if v < len(a):
         flag[v] = 1
-print flag
+print(flag)
 
 for i,v in enumerate(flag):
     if v==0:
-        print i
+        print(i)
         break
  
     
