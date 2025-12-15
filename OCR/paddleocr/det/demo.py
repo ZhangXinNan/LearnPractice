0968e4c4f056695066120fe51061a6134418688c
zhangxinnan <zhangxin19870504@163.com>
2025-12-15 13:47:19 +0800

    整理目录

--- OCR/paddleocr/det/demo.py
+++ OCR/paddleocr/det/demo.py
@@ -1,9 +1,13 @@
 
 from paddleocr import TextDetection
 model = TextDetection(model_name="PP-OCRv5_server_det")
-output = model.predict("general_ocr_001.png", batch_size=1)
+img_path = '/Users/zhangxin/gitlab/ocr_service/test/imgs/screenshot/华泰证券专业版3.png'
+# img_path = "general_ocr_001.png"
+output = model.predict(img_path, batch_size=1)
+print(type(output))
 for res in output:
-    res.print()
+    # res.print()
+    print(type(res))
     res.save_to_img(save_path="./output/")
     res.save_to_json(save_path="./output/res.json")
 
